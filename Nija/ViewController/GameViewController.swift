819dd3255ce6ad3842e9f0d8fb3a9bd3f4d0c49c
Nguyen Hoang Viet <anhvietmk05@gmail.com>
2021-08-16 14:52:54 +0700

    Đây là NIJAIOS

---
+++ Nija/ViewController/GameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewController.swift
+//  Nija
+//
+//  Created by Nguyen Hoang Viet on 15/08/2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Nija")
+        
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
